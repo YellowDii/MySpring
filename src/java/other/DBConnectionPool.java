7ee45e5e73d7a61f563611a48bc455a4fda9c946
YellowDii <781901120@qq.com>
2019-12-23 10:59:49 +0800

    参考书籍《Spring5核心原理与30个类手写实战》 开始我的架构学习以及Spring手写之路 2019.12.23 第一次提交 主要涉及到抽象工厂模式

---
+++ src/java/other/DBConnectionPool.java
@@ -0,0 +1,170 @@
+package java.other;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+//数据库链接池管理类
+public final class DBConnectionPool extends Pool {
+    private int checkedOut;//正在使用的连接数
+    private Vector<Connection> freeConnections = new Vector<Connection>();//存放产生的连接对 象容器
+    private String password = null; // 密码
+    private String url = null;//连接字符串
+    private String userName = null;//用户名
+    private static int num = 0;//空闲连接数
+    private static int numActive = 0;//当前可用的连接数
+    private static DBConnectionPool pool = null; //连接池实例变量
+
+
+    //产生数据连接池
+    public static synchronized DBConnectionPool getlnstance() {
+        if (pool == null) {
+            pool = new DBConnectionPool();
+        }
+        return pool;
+    }
+
+    //获得一个数据库连接池的实例
+    private DBConnectionPool() {
+        try {
+            init();
+            for (int i = 0; i < normalConnect; i++) { //初始 normalConn 个连接
+                Connection c = newConnection();
+                if (c != null) {
+                    freeConnections.addElement(c); //往容器中添加一个连接对象
+                    num++; //记录总连接数
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //初始化
+    private void init() throws IOException {
+        InputStream is = DBConnectionPool.class.getResourceAsStream(propertiesName);
+        Properties p = new Properties();
+        p.load(is);
+        this.userName = p.getProperty("userName");
+        this.password = p.getProperty("password");
+        this.driverName = p.getProperty("driverName");
+        this.url = p.getProperty("url");
+        this.driverName = p.getProperty("driverName");
+        this.maxConnect = Integer.parseInt(p.getProperty("maxConnect"));
+        this.normalConnect = Integer.parseInt(p.getProperty("normalConnect"));
+    }
+
+    //如果不再使用某个连接对象，可调此方法将该对象释放到连接池
+    public synchronized void freeConnection(Connection con) {
+        freeConnections.addElement(con);
+        num++;
+        checkedOut--;
+        numActive--;
+        notifyAll(); //解锁
+    }
+
+    //创建一个新连接
+    private Connection newConnection() {
+        Connection con = null;
+        try {
+            if (userName == null) { // 用户、密码都为空
+                con = DriverManager.getConnection(url);
+            } else {
+                con = DriverManager.getConnection(url, userName, password);
+            }
+            System.out.println("连接池创建一个新的连接");
+        } catch (SQLException e) {
+            System.out.println("创建这个 URL 的连接" + url);
+            return null;
+        }
+        return con;
+    }
+
+    //返回当前空闲连接数
+    public int getnum() {
+        return num;
+    }
+
+    //返回当前连接数
+    public int getnumActive() {
+        return numActive;
+    }
+
+    // (单例模式)获取一个可用连接
+    public synchronized Connection getConnection() {
+        Connection con = null;
+        if (freeConnections.size() > 0) { // 还有空闲的连接
+            num--;
+            con = (Connection) freeConnections.firstElement();
+            freeConnections.removeElementAt(0);
+            try {
+                if (con.isClosed()) {
+                    System.out.println("从连接池中删除一个无效连接");
+                    con = getConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("从连接池中删除一个无效连接");
+                con = getConnection();
+            }
+        } else if (maxConnect == 0 || checkedOut < maxConnect) {
+            // 没有空闲连接且当前连接小于最大允许值，最大值为0则不限制
+            con = newConnection();
+        }
+        if (con != null) { //当前连接数加1
+            checkedOut++;
+        }
+        numActive++;
+        return con;
+    }
+
+    //获取一个连接，并加上等待时间限制，单位为毫秒
+    public synchronized Connection getConnection(long timeout) {
+        long startTime = new Date().getTime();
+        Connection con;
+        while ((con = getConnection()) == null) {
+            try {
+                wait(timeout); // 线程等待
+            } catch (InterruptedException e) {
+            }
+            if ((new Date().getTime() - startTime) >= timeout) {
+                return null; //如果超时，则返回
+            }
+        }
+        return con;
+    }
+
+    //关闭所有连接
+    public synchronized void release() {
+        try {
+            //将当前连接赋值到枚举中
+            Enumeration allConnections = freeConnections.elements();
+            //使用循环关闭所用连接
+            while (allConnections.hasMoreElements()) {
+                //如果此枚举对象至少还有一个可提供的元素，则返回此枚举对象的下一个元素
+                Connection con = (Connection) allConnections.nextElement();
+                try {
+                    con.close();
+                    num--;
+                } catch (SQLException e) {
+                    System.out.println("羌必关闭连接池中的连接");
+                }
+            }
+            freeConnections.removeAllElements();
+            numActive = 0;
+        } finally {
+            super.release();
+        }
+    }
+
+    //建立连接池
+    public void createPool() {
+        pool = new DBConnectionPool();
+        if (pool != null) {
+            System.out.println("创建连接池成功");
+        } else {
+            System.out.println("创建连接池失败");
+        }
+    }
+}
